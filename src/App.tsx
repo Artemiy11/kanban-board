afb98867c49e378945a2494cb4dbc02e1ae74bbe
Artemiy11 <kartemiy678@gmail.com>
2023-06-06 17:37:59 +0200

    cleared unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,7 @@ import { observer } from "mobx-react";
 import Board from "./components/Board/Board";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-// import { useEffect } from "react";
-
-enum ColumnType {
-  TO_DO = "Todo",
-  IN_PROGRESS = "In Progress",
-  BLOCKED = "Blocked",
-  COMPLETED = "Completed",
-}
-
-interface DataItem {
-  id: number;
-  name: string;
-}
+import { ColumnType } from "./utils/enums";
 
 const App = observer(() => {
 
