d735cb96093250e44952152ccdb9dd67b590b265
Artemiy11 <kartemiy678@gmail.com>
2023-06-13 13:32:20 +0200

    vite.config modifed (https=false)

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  // Ваши другие настройки конфигурации...
+  define: {
+    'process.env.HTTPS': false
+  },
+  plugins: [react()]
+});
