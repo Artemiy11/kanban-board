5a8e968505532b64ef7cf37078ef6e08f4d04624
Artemiy11 <kartemiy678@gmail.com>
2023-06-14 19:03:32 +0200

    api url update

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Ваши другие настройки конфигурации...
-  define: {
-    'process.env.HTTPS': false
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://193.233.49.179:3002',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
   plugins: [react()]
 });
